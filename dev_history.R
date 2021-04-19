aacde04f731e95caf59265bf835552ee1e3949a9
nicolas manoharan <nicolas.manoharan@gmail.com>
2021-04-19 11:04:12 +0200

    Step 1 : Création du dev_history.R Step 2 : Initialisation de Git Step 3 : Licence agpl3 Step 4 : Mise en place intégration continue

--- dev_history.R
+++ dev_history.R
@@ -1,1 +1,24 @@
-usethis::use_build_ignore()
+## Step 1 : Création du dev_history.R
+# Exclusion du build
+
+usethis::use_build_ignore("dev_history.R")
+
+## Step 2  : Initialization de git
+
+usethis::use_git()
+
+# dans le terminal
+
+#git remote add origin git@github.com:nicolas95870/reporting_template.git
+# git push -u origin master
+
+
+## Step 3   : Licence,
+
+usethis::use_agpl3_license()
+
+#Modifier la description .
+
+devtools::check()
+
+
